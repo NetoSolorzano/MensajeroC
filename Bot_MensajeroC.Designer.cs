c352413c1915e4cee1da7ff60d1311bd4722c8e5
Lucio Solórzano <lucio.solorzano@gmail.com>
2022-02-16 15:10:37 -0500

    Agregar archivos de proyecto.

---
+++ Bot_MensajeroC.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MensajeroC
+{
+    partial class Bot_MensajeroC
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventos_MensajeroC = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventos_MensajeroC)).BeginInit();
+            // 
+            // eventos_MensajeroC
+            // 
+            this.eventos_MensajeroC.Log = "Application";
+            // 
+            // Bot_MensajeroC
+            // 
+            this.ServiceName = "Bot_MensajeroC";
+            ((System.ComponentModel.ISupportInitialize)(this.eventos_MensajeroC)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventos_MensajeroC;
+    }
+}
---
+++ Bot_MensajeroC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroC
+{
+    public partial class Bot_MensajeroC : ServiceBase
+    {
+        public Bot_MensajeroC()
+        {
+            InitializeComponent();
+            eventos_MensajeroC = new EventLog();
+            if (!EventLog.SourceExists("Bot_MensajeroC"))
+            {
+                EventLog.CreateEventSource("Bot_MensajeroC", "Application");
+            }
+            eventos_MensajeroC.Source = "Bot_MensajeroC";
+            eventos_MensajeroC.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // escribe en el log de eventos del sistema - inicio del mensajero
+            eventos_MensajeroC.WriteEntry("Inicio del Bot_MensajeroC");
+        }
+
+        protected override void OnStop()
+        {
+            // escribe en el log de eventos del sistema - detención del mensajero
+            eventos_MensajeroC.WriteEntry("Detención del Bot_MensajeroC");
+        }
+    }
+}
